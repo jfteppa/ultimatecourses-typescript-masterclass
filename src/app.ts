80a7ca26d530e126eb79d5993a50a527698c4c30
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-18 22:39:51 -0400

    Exploring “this” with .call, .apply and .bind

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
-// Function
-function myFunction() {
-  console.log('myFunction Function:::', this);
-  // this is my Window Object
-}
-myFunction();
-
 // Object Literal
 const myObj = {
   myObjectMethod() {
@@ -12,14 +5,61 @@ const myObj = {
     // this is my Custom Object
   },
 };
-myObj.myObjectMethod();
+// myObj.myObjectMethod();
 
-// Class
-class MyClass {
-  myClassMethod() {
-    console.log('myClassMethod Class:::', this);
-    // this is my MyClass
+// Function
+function myFunction(text: string, arg2: string) {
+  console.log('myFunction Function:::', this, text, arg2);
+  // this is my Window Object
+}
+myFunction('ABC', 'DEF');
+
+/**
+ * it will call the funciton
+ * but it changes context
+ */
+myFunction.call(myObj, 'call', 'DEF');
+
+myFunction.call([]);
+
+/**
+ * difference between call and apply is
+ * apply passes the arguments in array and call
+ * passes the arguments comma separated.
+ */
+myFunction.apply(myObj, ['apply', 'DEF']);
+
+/**
+ * c = call = comma
+ * a = apply = array
+ */
+
+/**
+ * .bind let us change the context ahead of time
+ * call and apply invoke the funciton,
+ * .bind does not call the function but returns a new bind function
+ *
+ */
+
+console.log('======================================');
+
+class myClass {
+  myCalssMethod2() {
+    console.log('myCalssMethod2', this);
   }
 }
-const myClassInstance = new MyClass();
-myClassInstance.myClassMethod();
+const bindFunction = myFunction.bind(myClass, 'bind', '123');
+
+myFunction.call(myObj, 'call', 'DEF');
+myFunction.apply(myObj, ['apply', 'DEF']);
+
+bindFunction(); // arguments does not change
+bindFunction('bind', '456'); // arguments does not change
+
+/**
+ * this way we can change the arguments after binding
+ * with the new context
+ */
+const bindFunction2 = myFunction.bind(myClass);
+bindFunction2();
+bindFunction2('bind', '789');
