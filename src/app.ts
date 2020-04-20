5a2e1583de4c74729cc7ee1a65ee04489fc7f586
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-19 22:43:21 -0400

    “Pick” Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -1,62 +1,25 @@
 interface Person {
   name: string;
-  age?: number;
+  age: number;
+  address: {};
+  gender: string;
 }
 
-const person: Person = {
-  name: 'Juan',
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
 };
 
-function printAge(person: Person) {
-  return `${person.name} is ${person.age}`;
-}
-const age = printAge(person);
-console.log(age);
-
-type MyRequired<T> = {
-  // [P in keyof T]?: T[P];
-  // both are the same.
-  // [P in keyof T]+?: T[P];
-
-  // we have the + to add
-  // and the - to remove
-  [P in keyof T]-?: T[P];
-
-  // -readonly [P in keyof T]-?
-  // +readonly [P in keyof T]+?
-};
-
-function printAge2(person: MyRequired<Person>) {
-  return `${person.name} is ${person.age}`;
-}
-
-// person.age = 10;
-// const age2 = printAge2(person); // error
-
-const person2: MyRequired<Person> = {
+const person: MyPick<Person, 'name' | 'age'> = {
   name: 'Juan',
   age: 33,
 };
 
-const age2 = printAge2(person2);
-console.log(age2);
-
-// TS Built-in
-function printAge3(person: Required<Person>) {
-  return `${person.name} is ${person.age}`;
-}
-
-/**
- * Property 'age' is missing in type '{ name: string; }'
- * but required in type 'Required<Person>'
- */
-/* const person3: Required<Person> = {
-  name: 'Juan'
-}; */
-
-const person4: Required<Person> = {
+// Built-in Pick<T>
+// with Pick we just tell what properties we want
+const perso2: Pick<Person, 'name' | 'gender'> = {
   name: 'Juan',
-  age: 23,
+  gender: 'Male',
 };
-const age4 = printAge3(person4);
-console.log(age4);
+
+// with partial we make all the properties optional
+const person3: Partial<Person> = {};
