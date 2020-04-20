fc69b07ed632c170d7fc9dc5f2641396fdbe09ff
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-19 20:28:37 -0400

    “keyof” Index Type Queries

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,3 @@
-console.log(typeof ''); // string
-console.log(typeof []); // object
-
-// inferring types (hover on object const)
 const person = {
   name: 'Teppa',
   age: 33,
@@ -9,22 +5,44 @@ const person = {
 
 type Person = typeof person;
 
-// JavaScript > typeof person == object
-console.log(typeof person);
-
-/**
- * TypeScript will inferring the type of object person. (hover on type Person)
- * Remember this is for TypeScript, not the js compiled.
- * Just to get move features for development.
- */
-
 const anotherPerson: Person = {
   name: 'John',
   age: 30,
-  // gender: 'male' // this will give us error
 };
 
-const anotherPerson2: typeof person = {
-  name: 'John',
-  age: 30,
-};
+/**
+ * Union of keys, in this case "name" | "age" (hover on PersonKeys)
+ * again only on ts not js.
+ * "name" or "age" are not types but I can see the keys
+ */
+type PersonKeys = keyof Person;
+
+/**
+ * this will give us the union of the types of the keys
+ * in this case string | number (hover on PersonTypes)
+ */
+type PersonTypes = Person[PersonKeys];
+
+// const anotherPerson2: PersonKeys = {};
+
+// const anotherPerson3: PersonTypes = {};
+
+class Aux {
+  public arg1: PersonKeys;
+  public arg2: PersonTypes;
+  public arg3: number;
+  public arg4: string;
+}
+
+const auxInst = new Aux();
+// auxInst.arg1 = 1; // "name" or "age" are not types
+// auxInst.arg1 = 'string'; // "name" or "age" are not types
+
+auxInst.arg2 = 1; // string | number
+auxInst.arg2 = 'string'; // string | number
+
+auxInst.arg3 = 1;
+// auxInst.arg3 = 'string'; // only umber
+
+// auxInst.arg4 = 1; // only string
+auxInst.arg4 = 'string';
