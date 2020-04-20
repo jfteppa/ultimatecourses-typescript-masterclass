6e08b9e123453c5f05f8aef52e736baa9fcc98ab
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-19 20:12:41 -0400

    “typeof” Type Queries.

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,30 @@
-const elem = document.querySelector('.click');
+console.log(typeof ''); // string
+console.log(typeof []); // object
 
-function handleClick(event: Event) {
-  event.preventDefault();
-  console.log('handleClick this', this);
-  /**
-   * at this point "this" is the dom element and we can try something like
-   * this.href but when hovering the "this" it will be type any
-   *
-   * We change the tsconfig and add the noImplicitThis.
-   * after this the "this" will be red
-   */
-}
+// inferring types (hover on object const)
+const person = {
+  name: 'Teppa',
+  age: 33,
+};
 
-// elem.addEventListener('click', handleClick, false);
-elem.addEventListener('click', handleClick2, false);
+type Person = typeof person;
+
+// JavaScript > typeof person == object
+console.log(typeof person);
 
 /**
- * typing "this"
- * it will look like an argument
- * however it is not an argument of the function
- * and it will not affect the other arguments
- *
- * when typing the "this" and hovering on it
- * it will show more info and when looking for its elements or functions
- * with "." we will be able to se them
- *
- * event still the 1st actually argument.
+ * TypeScript will inferring the type of object person. (hover on type Person)
+ * Remember this is for TypeScript, not the js compiled.
+ * Just to get move features for development.
  */
-function handleClick2(this: HTMLAnchorElement, event: Event) {
-  event.preventDefault();
-  console.log('handleClick2 this.href', this.href);
-  console.log('handleClick2 this.className', this.className);
-}
+
+const anotherPerson: Person = {
+  name: 'John',
+  age: 30,
+  // gender: 'male' // this will give us error
+};
+
+const anotherPerson2: typeof person = {
+  name: 'John',
+  age: 30,
+};
