e501a123dda9897fe83362bc1057176f0136bc35
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-19 23:14:01 -0400

    “Record” Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,60 @@
-interface Person {
-  name: string;
-  age: number;
-  address: {};
-  gender: string;
+let dictionary: { [key: string]: any } = {};
+
+interface TrackStates {
+  current: string;
+  next: string;
 }
 
-type MyPick<T, K extends keyof T> = {
-  [P in K]: T[P];
+const item: TrackStates = {
+  current: '123',
+  next: '456',
 };
 
-const person: MyPick<Person, 'name' | 'age'> = {
-  name: 'Juan',
-  age: 33,
+/**
+ * if we look at the type definition
+ * the key is a string
+ * but we asigned to be number zero (0)
+ * but this is not TS this is how JS indexes
+ * and it will actually convert the number into string
+ */
+
+// Numbers are coerced/change type to String
+dictionary[0] = item; // == dictionary['0'] = item;
+
+// Built-in
+/*
+  // Construct a type with a set of properties K of type T
+
+  // 1st arg generic type
+  // K is a subtype of the generic string
+  // 
+  type Record<K extends keyof any, T> = {
+      [P in K]: T;
+  };
+ */
+
+let dictionary2: Record<string, TrackStates> = {};
+
+// we can make it more restrictive and not use the interface and only use values
+const item2: Record<'current' | 'next', string> = {
+  current: '789',
+  next: '012',
+};
+// item2.test = 'a';
+
+// we can also use the interface
+const item3: Record<keyof TrackStates, string> = {
+  current: '345',
+  next: '678',
 };
 
-// Built-in Pick<T>
-// with Pick we just tell what properties we want
-const perso2: Pick<Person, 'name' | 'gender'> = {
-  name: 'Juan',
-  gender: 'Male',
-};
+// if we do not have access to the TrackStates interface we can do this
+let dictionary3: Record<string, typeof item3> = {};
 
-// with partial we make all the properties optional
-const person3: Partial<Person> = {};
+dictionary[1] = item2;
+dictionary[2] = item3;
+
+console.log(item);
+console.log(item2);
+console.log(item3);
+console.log(dictionary);
