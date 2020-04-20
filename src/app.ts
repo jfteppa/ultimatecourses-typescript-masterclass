a59a351bb293fe6baffe85ab64f2e026cd8a6332
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-19 21:50:37 -0400

    “Partial” Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -1,92 +1,51 @@
 interface Person {
   name: string;
   age: number;
-  gender: string;
 }
 
-interface ReadOnlyPerson {
-  readonly name: string;
-  readonly age: number;
+interface PartialPerson {
+  name?: string;
+  age?: number;
+}
+
+function updatePerson(person: Person, prop: PartialPerson) {
+  return { ...person, ...prop };
 }
 
 const person: Person = {
   name: 'Juan',
   age: 33,
-  gender: 'male',
 };
 
-/**
- * lets make our obj inmutable
- * const does not let you reasign
- * but it lets you change/mutate it's property values
- */
+const newPerson = updatePerson(person, { name: 'ABC' });
+console.log('newPerson', newPerson);
 
-person.name = 'ABC';
-
-function freezePerson(person: Person): ReadOnlyPerson {
-  return Object.freeze(person);
-}
-
-// newPerson: ReadOnlyPerson
-const newPerson = freezePerson(person);
-
-// Cannot assign to 'name' because it is a read-only property.
-// newPerson.name = 'ABC';
-// newPerson.gender we need to add it to the ReadOnlyPerson interface
-
-/**
- * we don't need the interface to return a readonly object,
- * if we add another property this will take care of it
- * with the interface we need to declare all of them
- */
-function freezePerson2(person: Person) {
-  return Object.freeze(person);
-}
-
-// const newPerson2: Readonly<Person>
-const newPerson2 = freezePerson2(person);
-
-// newPerson2.name = 'ABC';
-
-// it takes care of all of the properties in the object
-// newPerson2.gender = 'female';
-
-// we can rewrite the function to be more generic
-
-function freezeObj(obj: object) {
-  return Object.freeze(obj);
-}
-
-// let newPerson3: object
-const newPerson3 = freezeObj(person);
-
-// with the example above we don't know the type of newPerson3
-
-/**
- *
- * getting the type of the object
- * we know freezePerson2 returns Readonly<Person>
- * we need to return Readonly<ObjectType>
- * We are mapping the return object to the type
- * of the received object
- */
-function freezeObjType<T>(obj: T): Readonly<T> {
-  return Object.freeze(obj);
-}
-
-// const newPerson4: Readonly<Person>
-const newPerson4 = freezeObjType(person);
-
-type MyReadOnly<ObjectType> = {
-  readonly [ObjProperty in keyof ObjectType]: ObjectType[ObjProperty];
+type MyPartial<T> = {
+  [P in keyof T]?: T[P];
 };
 
-function freeze<T>(obj: T): MyReadOnly<T> {
-  return Object.freeze(obj);
+function updatePerson2(person: Person, prop: MyPartial<Person>) {
+  return { ...person, ...prop };
 }
 
-// const np: MyReadOnly<Person>
-const np = freeze(person);
+const newPerson2 = updatePerson2(person, { name: 'Juan Felipe' });
+console.log('newPerson2', newPerson2);
 
-// Cannot assign to 'age' because it is a read-only property.ts(2540)
-// np.age = 123
+// TS BuiltIn
+
+function updatePerson3(person: Person, prop: Partial<Person>) {
+  return { ...person, ...prop };
+}
+
+const newPerson3 = updatePerson3(person, { name: 'Juan Felipe 2' });
+console.log('newPerson3', newPerson3);
+
+// Generic
+function updateObj<T>(obj: T, prop: Partial<T>) {
+  return { ...obj, ...prop };
+}
+
+const newPerson4 = updateObj(person, { name: 'Juan Felipe 3' });
+
+// gender does not exist
+// const newPerson5 = updateObj(person, { gender: 'female' });
