b5f2408035235e749f9106f1adcd5a4123cdff83
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-21 00:44:30 -0400

    Literal Type Guards and “in” Operator

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,53 @@
+const exists = 'localStorage' in window;
+console.log('localStorage in window: ', exists);
+// it checks if the object has a property
+// it does not matter if the property value is false or null
+const win = {
+  f: false,
+  n: null,
+};
+console.log('f' in win);
+console.log('n' in win);
+
 class Song {
+  // literal type
+  // type 'song'
+  // (property) Song.kind: "song"
+  kind: 'song';
+
+  // type 'string'
+  // (property) Song.kind2: string
+  kind2 = 'song';
+
   constructor(public title: string, public duration: number) {}
 }
 
 class Playlist {
+  kind: 'playlist';
+  kind2 = 'playlist';
   constructor(public name: string, public songs: Song[]) {}
 }
 
-/**
- *
- * User Defined Type Guard
- *
- * always return a boolean
- * but it will supply further type information
- * it is a reusable function
- *
- */
 function isSong(item: any): item is Song {
   return item instanceof Song;
 }
 
+function isSong2(item: any): item is Song {
+  return 'title' in item;
+}
+
 function getItemName(item: Song | Playlist) {
+  if (item.kind2 === 'song') {
+    // (parameter) item: Song | Playlist
+    // return item.title; // error
+  }
+  if (item.kind === 'song') {
+    // (parameter) item: Song
+    return item.title;
+  }
+  if (isSong2(item)) {
+    return item.title;
+  }
   if (isSong(item)) {
     return item.title;
   }
