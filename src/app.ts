b401847aa9c423114a030531e4b205dfa90dbb98
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-20 23:38:46 -0400

    “typeof” and Type Guards.

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,54 @@
-let dictionary: { [key: string]: any } = {};
+function foo(bar: string | number) {
+  // functions available
+  // bar.toString
+  // bar.valueOf
 
-interface TrackStates {
-  current: string;
-  next: string;
+  if (typeof bar === 'string') {
+    // string; functions available
+    // bar.toLowerCase
+    // bar.substring
+    return bar.toUpperCase();
+  }
+  // bar.toExponential
+  // it needs the return or to have the else block to be able to see the options.
+  // outside of the conditional block
+  return bar.toFixed(2);
 }
 
-const item: TrackStates = {
-  current: '123',
-  next: '456',
-};
+class Song {
+  // constructor(public title: string, public duration: string | number) {}
+  constructor(public title: string, public duration: string | number) {}
+}
+
+function getSongDuration(item: Song) {
+  if (typeof item.duration === 'string') {
+    return item.duration;
+  }
+  const { duration } = item;
+  const minutes = Math.floor(duration / 60000);
+  const seconds = (duration / 1000) % 60;
+  return `${minutes}:${seconds}`;
+}
+
+const songDurationFromString = getSongDuration(
+  new Song('Wonderful wonderful', '05:31')
+);
+
+console.log(songDurationFromString);
+
+const songDurationFromMS = getSongDuration(
+  new Song('Wonderful wonderful', 330000)
+);
+
+console.log(songDurationFromMS);
 
 /**
- * if we look at the type definition
- * the key is a string
- * but we asigned to be number zero (0)
- * but this is not TS this is how JS indexes
- * and it will actually convert the number into string
+ * the idea of this Type guard is for us to have the functions of the types
+ * on TS after the passing 1 conditional
+ * in this example the property can be string or number
+ * if the property is a string then inside of the logic block it will be a string
+ * and we can access all of the string functions
+ * if it is not a string then (in this example) it is a number so we have
+ * all the functions of a number, with TS there is no need then to make them to be
+ * a string or number to have the functions for each type
  */
-
-// Numbers are coerced/change type to String
-dictionary[0] = item; // == dictionary['0'] = item;
-
-// Built-in
-/*
-  // Construct a type with a set of properties K of type T
-
-  // 1st arg generic type
-  // K is a subtype of the generic string
-  // 
-  type Record<K extends keyof any, T> = {
-      [P in K]: T;
-  };
- */
-
-let dictionary2: Record<string, TrackStates> = {};
-
-// we can make it more restrictive and not use the interface and only use values
-const item2: Record<'current' | 'next', string> = {
-  current: '789',
-  next: '012',
-};
-// item2.test = 'a';
-
-// we can also use the interface
-const item3: Record<keyof TrackStates, string> = {
-  current: '345',
-  next: '678',
-};
-
-// if we do not have access to the TrackStates interface we can do this
-let dictionary3: Record<string, typeof item3> = {};
-
-dictionary[1] = item2;
-dictionary[2] = item3;
-
-console.log(item);
-console.log(item2);
-console.log(item3);
-console.log(dictionary);
