5a3ca2bea150d27943ea64de2e641f3c684befc6
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-21 22:53:41 -0400

    Function Generics.

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,76 @@
-interface Artist {
-  name: string;
+class List {
+  private list: any[];
+
+  addItem(item: any): void {
+    this.list.push(item);
+  }
+
+  getList(): any[] {
+    return this.list;
+  }
 }
 
-class ArtistCreator implements Artist {
-  constructor(public name: string) {}
+class Pizza {
+  constructor(private name: string, private price: number) {}
 }
 
-function artistFactory({ name }: Artist) {
-  return new ArtistCreator(name);
+// const list: List
+const list = new List();
+
+list.addItem(new Pizza('Pepperoni', 15));
+
+// const pizzas: any[]
+const pizzas = list.getList();
+
+class List2<T> {
+  private list: T[];
+
+  addItem(item: T): void {
+    this.list.push(item);
+  }
+
+  getList(): T[] {
+    return this.list;
+  }
 }
 
-// const artist: ArtistCreator
-const artist = artistFactory({ name: 'Juan' });
+// const list2: List2<Pizza>
+const list2 = new List2<Pizza>();
 
-class ArtistCreator2 {
-  constructor(public name: string) {}
-}
+list2.addItem(new Pizza('Pepperoni', 15));
 
-function artistFactory2({ name }: ArtistCreator2) {
-  return new ArtistCreator(name);
-}
-
-// const artist2: ArtistCreator
-const artist2 = artistFactory2({ name: 'Juan Felipe' });
+// const pizzas2: Pizza[]
+const pizzas2 = list2.getList();
 
 /**
- * if we need to have some instance or implementation we would go with the class
- * if we just need to type check and not implement any methods or instantiate objects
- * then we can just create an interface
+ * Argument of type '{ name: string; price: number; }' is not assignable to parameter of type 'Pizza'.
+  Property 'name' is private in type 'Pizza' but not in type '{ name: string; price: number; }'.ts(2345)
  */
+// list2.addItem({ name: 'Pepperoni3', price: 15 });
+
+const obj = {
+  name: 'Pepperoni2',
+  price: 15,
+};
+
+/**
+ * Argument of type '{ name: string; price: number; }' is not assignable to parameter of type 'Pizza'.
+  Property 'name' is private in type 'Pizza' but not in type '{ name: string; price: number; }'.ts(2345)
+ */
+// list2.addItem(obj);
+
+class Coupon {
+  constructor(private name: string) {}
+}
+
+const anotherList = new List2<Coupon>();
+
+anotherList.addItem(new Coupon('PIZZA25'));
+
+/**
+ * Argument of type 'Pizza' is not assignable to parameter of type 'Coupon'.
+ */
+// anotherList.addItem(new Pizza('Pepperoni', 15));
+
+// const coupons: Coupon[]
+const coupons = anotherList.getList();
