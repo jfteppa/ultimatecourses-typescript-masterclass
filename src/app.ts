17634138a57db85ea565b86faae470e8e272f447
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-22 01:25:19 -0400

    Writing Declaration Files.

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,11 @@
-const enum Sizes {
-  Small = 'small',
-  Medium = 'medium',
-  // Large = 3, this is also possible, to mix
-  Large = 'large',
-}
-
-// compiles into
-/**
-  var Sizes;
-  (function (Sizes) {
-      Sizes["Small"] = "small";
-      Sizes["Medium"] = "medium";
-      Sizes["Large"] = "large";
-  })(Sizes || (Sizes = {}));
- */
-
-let selected: Sizes;
-console.log(selected);
-
-function updateSize(size: Sizes): void {
-  selected = size;
-}
-
-updateSize(Sizes.Large);
-console.log('selected', selected);
-
-let selected2: Sizes = Sizes.Small;
-console.log(selected2);
-
-function updateSize2(size: Sizes): void {
-  selected2 = size;
-}
-
-updateSize2(Sizes.Large);
-
-console.log('selected2', selected2);
+import * as _ from 'lodash';
 
 /**
- * the enum function in js is written for us
- * but if we want to avoid all this code generated
- * we can use an inline member
- * we just make the enum as a const
+ * This is the information that comes from the @types/lodash when the @types/lodash was installed
+ *
+ * chunk(array: _.List<unknown> | null | undefined, size?: number | undefined): unknown[][]
+ * The array to process. Creates an array of elements split into groups the length of size.
+ * If collection can’t be split evenly, the final chunk will be the remaining elements.
+ * @return — Returns the new array containing chunks.
  */
-
-enum Sizes2 {
-  Small = 'small',
-  Medium = 'medium',
-  Large = 'large',
-}
-
-// will not generate the code
-/**
-  var Sizes2;
-  (function (Sizes2) {
-      Sizes2["Small"] = "small";
-      Sizes2["Medium"] = "medium";
-      Sizes2["Large"] = "large";
-  })(Sizes2 || (Sizes2 = {}));
- */
+// _.chunk();
