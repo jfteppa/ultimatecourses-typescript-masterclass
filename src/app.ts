fdb2a166cf159673747dc26e19fa9cf60ddee2b0
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-21 23:06:38 -0400

    Function Overloads.

--- src/app.ts
+++ src/app.ts
@@ -1,76 +1,23 @@
-class List {
-  private list: any[];
-
-  addItem(item: any): void {
-    this.list.push(item);
+function reverse(str: string): string;
+// function reverse(arr: any[]): any[];
+function reverse<T>(arr: T[]): T[];
+function reverse<T>(something: string | T[]): string | T[] {
+  if (typeof something === 'string') {
+    return something.split('').reverse().join('');
   }
-
-  getList(): any[] {
-    return this.list;
-  }
+  // with slice we are making a copy and returning a new
+  return something.slice().reverse();
 }
 
-class Pizza {
-  constructor(private name: string, private price: number) {}
-}
+reverse('Pepperoni');
+reverse(['Bacon', 'Pepperoni', 'Chili']);
 
-// const list: List
-const list = new List();
+// 1/2 reverse(str: string): string
+// 2/2 reverse(arr: any[]): any[]
+// reverse();
 
-list.addItem(new Pizza('Pepperoni', 15));
+// function reverse<string>(arr: string[]): string[] (+1 overload)
+reverse(['Bacon', 'Pepperoni', 'Chili']);
 
-// const pizzas: any[]
-const pizzas = list.getList();
-
-class List2<T> {
-  private list: T[];
-
-  addItem(item: T): void {
-    this.list.push(item);
-  }
-
-  getList(): T[] {
-    return this.list;
-  }
-}
-
-// const list2: List2<Pizza>
-const list2 = new List2<Pizza>();
-
-list2.addItem(new Pizza('Pepperoni', 15));
-
-// const pizzas2: Pizza[]
-const pizzas2 = list2.getList();
-
-/**
- * Argument of type '{ name: string; price: number; }' is not assignable to parameter of type 'Pizza'.
-  Property 'name' is private in type 'Pizza' but not in type '{ name: string; price: number; }'.ts(2345)
- */
-// list2.addItem({ name: 'Pepperoni3', price: 15 });
-
-const obj = {
-  name: 'Pepperoni2',
-  price: 15,
-};
-
-/**
- * Argument of type '{ name: string; price: number; }' is not assignable to parameter of type 'Pizza'.
-  Property 'name' is private in type 'Pizza' but not in type '{ name: string; price: number; }'.ts(2345)
- */
-// list2.addItem(obj);
-
-class Coupon {
-  constructor(private name: string) {}
-}
-
-const anotherList = new List2<Coupon>();
-
-anotherList.addItem(new Coupon('PIZZA25'));
-
-/**
- * Argument of type 'Pizza' is not assignable to parameter of type 'Coupon'.
- */
-// anotherList.addItem(new Pizza('Pepperoni', 15));
-
-// const coupons: Coupon[]
-const coupons = anotherList.getList();
+// function reverse<number>(arr: number[]): number[] (+1 overload)
+reverse([1, 2, 3]);
