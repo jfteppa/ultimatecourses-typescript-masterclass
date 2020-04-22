07cb047148a0af910798f20a655b948ee3e99d59
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-22 19:10:34 -0400

    Augmenting Modules with Declarations.

--- src/@types/lodash/index.d.ts
+++ src/@types/lodash/index.d.ts
@@ -1,3 +1,13 @@
+import * as lodash from 'lodash';
+
 declare module 'lodash' {
-  export function chunk(collection: any, size?: number): any[][];
+  /**
+   * because LoDashStatic is an interface
+   * we can merge declarations of an interface
+   * in other words redeclare the interface (same name)
+   * with new information, in this case the new log function
+   */
+  interface LoDashStatic {
+    log(item: string): void;
+  }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,36 @@
-/**
- * Could not find a declaration file for module 'lodash'.
- * implicitly has an 'any' type.
- * Try `npm install @types/lodash` if it exists or
- * add a new declaration (.d.ts) file containing `declare module 'lodash';
- */
+// module "lodash"
 import * as _ from 'lodash';
 
+// INFORMATION FROM @types/lodash
 /**
- * this function does not exist but because there is no types
- * it ts does not gives errors.
+ * LoDashStatic.chunk<number>(array: _.List<number> | null | undefined, size?: number | undefined): number[][]
  */
-// any
-// _.abc();
+_.chunk([1, 2, 3, 4], 2);
 
-// any
-// _.chunk();
-// the function chunk is a function on the lodash module
-// but without the types we don't get any information of the function
+// Property 'log' does not exist on type 'LoDashStatic'.
+// _.log();
 
-// we will create a type declaration file that gives mor info
-// at @types/lodash/index.d.ts
+// to add it
 
-// function chunk(collection: any, size?: number | undefined): any[][]
-// _.chunk
+_.mixin({
+  log(item: string) {
+    console.log(':::', item);
+  },
+});
+
+// still the error after adding the function with the mixin
+// Property 'log' does not exist on type 'LoDashStatic'.
+_.log('Hello');
+
+/***
+ * LoDashStatic is an interface
+ * in order to add type information we do so
+ * by augmenting the module at the custom declartion
+ * src/@types/lodash/index.d.ts
+ */
+
+// (method) LoDashStatic.log(item: string): void
+_.log('Hello');
+
+// Argument of type '123' is not assignable to parameter of type 'string'.
+// _.log(123);
