1661ac9ee27b3b16045f2d72deaec1ae763e7f22
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-23 01:16:28 -0400

    Understanding “lib” and ES libraries.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-export class Person {
-  constructor(private name: string) {}
-  getName() {
-    return this.name;
-  }
-}
+Array.isArray([]);
+
+// this should not exist on type ArrayConstructor
+Array.from(document.querySelectorAll('*'));
+/**
+ * Cannot find name 'document'.
+ * Do you need to change your target library?
+ * Try changing the `lib` compiler option to include 'dom'
+ */
