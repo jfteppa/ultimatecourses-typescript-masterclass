60ae13f54c54264ffd216cac287203383535391b
Juan Felipe Teppa <jteppa@rootstack.com>
2020-04-23 00:36:18 -0400

    Emitting Source Maps.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-console.log('Index');
-// with noImplicitAny: true, and noEmitOnError: true
-// because message implicitly has an 'any' type
-// the dist folder is not generated
-function log(message) {}
+class Person {
+  name: string = 'Teppa';
+}
